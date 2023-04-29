856223d8ac209cfc7dcd552d0130e7e8bd044df8
Danilomr305 <danilomr305@gmail.com>
2023-04-29 00:21:26 -0300

    EXERCICIOO JAVASCRIPT

--- atividades/ex 014/script.js
+++ atividades/ex 014/script.js
@@ -5,4 +5,17 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 12) {
+        /*BOM DIA*/
+        img.src = 'foto-manha.jpg'
+        document.body.style.background = '#e2cd9f'
+    }else if (hora >= 12 && hora <= 18) {
+        /*BOA TARDE */
+        img.src = 'foto-tarde.jpg'
+        document.body.style.background = '#b9846f'
+    }else {
+        /*BOA NOITE*/
+        img.src = 'foto-noite.jpg'
+        document.body.style.background = '#515154'
+    }
 }
