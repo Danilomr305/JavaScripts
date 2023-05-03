341dffe72fb6e5691c1766c1bfcfb9d0171eb471
Danilomr305 <danilomr305@gmail.com>
2023-05-03 16:08:56 -0300

    exercicio 2

--- atividades/ex 016/script.js
+++ atividades/ex 016/script.js
@@ -6,17 +6,21 @@ function contar() {
    let res = document.getElementById('res')
 
    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-    window.alert('[ERRO] Faltam dados!')
+    res.innerHTML = 'Impossivel contar'
    }else {
       res.innerHTML= ' Contando: '
       let i = Number(ini.value)
       let f = Number(fim.value)
       let p = Number(passo.value)
-    if (i < f){
+      if (p <= 0) {
+          window.alert('Passo inválido! Considerando PASSO 1')
+          p = 1
+      }
+      if (i < f){
         for(let c = i; c <= f; c += p) {
             res.innerHTML += `${c} \u{1f449}`}
             res.innerHTML += `\u{1f3c1}`
-    }else {
+      }else {
         for(let c = i; c >= f; c -= p) {
             res.innerHTML += `${c} \u{1f449}`}
             res.innerHTML= `\u{1f3c1}`
--- atividades/ex 017/script.js
+++ atividades/ex 017/script.js
@@ -1,51 +1,18 @@
- window.alert('Boa Tarde!')
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.getElementById('res')
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('Verifique os dados e tente novamente')
-    } else {
-        var fsex= document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        res.innerHTML = `idade calculada ${idade}`
-        var gênero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-        if (fsex[0].checked) {
-            gênero = 'Homem'
-            if (idade >=0 && idade < 14) {
-                /*criança*/
-                img.setAttribute('src', 'criança-menino.jpg')
-            } else if (idade < 23) {
-                /*jovens*/
-                img.setAttribute('src', 'homem-jovem.jpg')
-            }else if (idade < 65) {
-                /*adulto*/
-                img.setAttribute('src', 'homem-adulto.jpg')
-            } else {
-                /*idoso*/
-                img.setAttribute('src', 'homem-idoso.jpg')
-            }
-        } else if (fsex[1].checked) {
-            gênero = 'Mulher'
-            if (idade >=0 && idade < 14) {
-                    /*criança*/
-                    img.setAttribute('src', 'criança-menina.jpg')
-            } else if (idade < 23) {
-                    /*jovens*/
-                    img.setAttribute('src', 'mulher-jovem.jpg')
-            }else if (idade < 65) {
-                    /*adulta*/
-                    img.setAttribute('src', 'mulher-adulta.jpg')
-            } else {
-                    /*idosa*/
-                    img.setAttribute('src', 'mulher-idosa.jpg')
-            }
+ function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    }else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
         }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
-        res.appendChild (img)
     }
-}
+ }
