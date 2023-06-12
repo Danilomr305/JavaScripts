26fb8e5265ecea1ba6dac56fc8d659314071b514
Danilomr305 <danilomr305@gmail.com>
2023-06-12 13:34:46 -0300

    .

--- matheus battisti/26_removendo_espaços_duplos/js/js/script.js
+++ matheus battisti/26_removendo_espaços_duplos/js/js/script.js
@@ -1,1 +1,13 @@
 var str = "Esta string tem alguns espaços desnecessários!";
+
+console.log(str);
+
+console.log(str.replace(/( )+/g, " "));
+
+function removerEspacosDuplicados(str) {
+    return str.replace(/( )+/g, " ");
+
+}
+
+console.log(removerEspacosDuplicados("Olá      Mundo!"));
+console.log(removerEspacosDuplicados("A função    funcionou mesmo!"))
