fef9aba89855530b23597074c997418dbd695977
Danilomr305 <danilomr305@gmail.com>
2023-06-14 12:39:34 -0300

    .

---
+++ matheus battisti curso/08_tipos_de_dados=undefined_e_null/js/script.js
@@ -0,0 +1,12 @@
+//Hoisting - içamento
+console.log(sobrenome); //colocado antes da variavel undefined
+
+var nome = null; 
+var sobrenome = "Martins";
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = "Danilo";
+
+console.log(nome);
