3be8841a69dc5f8bcfccbb911147163c5706b433
Danilomr305 <danilomr305@gmail.com>
2023-06-14 11:43:50 -0300

    strings

---
+++ matheus battisti curso/06_tipos_de_dados=string/js/script.js
@@ -0,0 +1,24 @@
+var nome = "Danilo";
+var sobrenome = "Martins";
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof "asd");
+
+console.log(nomeCompleto);
+
+console.log(typeof "5.292929");
+
+var frase = 'Esta é uma frase complexa';
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.write('Ele disse:"Oá"');
+
+console.log("Este número " + nome);
