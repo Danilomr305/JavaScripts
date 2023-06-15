8724dac6cff05c514ccde8431f9efd8cb327e17f
Danilomr305 <danilomr305@gmail.com>
2023-06-14 22:55:23 -0300

    array

---
+++ matheus battisti curso/10_tipos_de_dados+Arrays/ja/script.js
@@ -0,0 +1,11 @@
+var array = [19, "Danilo", true, {teste: 5, teste: 3}];
+
+console.log(array);
+
+var array2 = [1, 2, 3, 4, 5, 6];
+
+console.log(array2);
+console.log(array2[4]);
+
+console.log(typeof array);
+console.log(typeof array2);
