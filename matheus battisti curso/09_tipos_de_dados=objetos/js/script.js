e36c7b70cfecea378c68913f1e0a72f592698a81
Danilomr305 <danilomr305@gmail.com>
2023-06-14 22:41:50 -0300

    objeto

---
+++ matheus battisti curso/09_tipos_de_dados=objetos/js/script.js
@@ -0,0 +1,17 @@
+var obj = {
+    nome: "Danilo",
+    idade: 19,
+    profissao: "programador",
+};
+
+console.log(obj);
+
+console.log(typeof obj);
+
+console.log(obj.nome);
+console.log(obj.idade);
+console.log(obj.profissao);
+
+console.log("O meu nome é " + obj.nome);
+console.log("A minha idade é " + obj.idade);
+console.log("A minha profissão é " + obj.profissao);
