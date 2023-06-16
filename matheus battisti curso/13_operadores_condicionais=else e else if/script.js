6bf44f2b9349623b275cf6a6090b8138f7b750c2
Danilomr305 <danilomr305@gmail.com>
2023-06-16 13:39:57 -0300

    .

--- matheus battisti curso/13_operadores_condicionais=else e else if/script.js
+++ matheus battisti curso/13_operadores_condicionais=else e else if/script.js
@@ -2,7 +2,19 @@ var nome = "Danilo";
 
 if(nome == "Pedro")  {
     console.log("O nome dele é Pedro");
-} else {
-    console.log("Ele possui outro nome!")
+} else if(nome == "Danilo") {
+    console.log("O nome é Danilo")
+} else if(nome == "Xavier") {
+    console.log("O nome é XAvier");
 
+}else {
+    console.log("Ele possui outro nome!")
+}
+
+var idade = 19;
+
+if(idade > 20) {
+    console.log("Ele pode entrar na festa!")
+} else if (idade >= 18) {
+    console.log("Ele só pode entrar com autorização")
 }
