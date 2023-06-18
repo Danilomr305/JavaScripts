5034792d9fdf5a67dad97e1f91a2558ce3845d51
Danilomr305 <danilomr305@gmail.com>
2023-06-18 11:36:41 -0300

    .

---
+++ matheus battisti curso/15_operadores_logicos/js/script.js
@@ -0,0 +1,8 @@
+var idade = 17;
+var nome = 'João';
+
+ if(nome == "João"  &&  idade == 16) {
+    console.log("O João pode entrar na aula de esgrima");
+ } else{
+    console.log("Esse não é o João!");
+ }
