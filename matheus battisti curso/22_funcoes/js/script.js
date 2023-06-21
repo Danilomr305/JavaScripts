f9062bfc402bc9ad5583e46e2b30bf8b2b547f7e
Danilomr305 <danilomr305@gmail.com>
2023-06-21 12:24:30 -0300

    .

---
+++ matheus battisti curso/22_funcoes/js/script.js
@@ -0,0 +1,35 @@
+function primeiraFuncao() {
+   
+    console.log("HEllo World das funções");
+}
+
+primeiraFuncao();
+
+function dizerNome(nome) {
+
+    console.log("O nome é: " + nome);
+
+}
+
+dizerNome("Danio");
+dizerNome("Dudu");
+dizerNome("Leo");
+
+var nomeDoBancoDeDados = "Dandan";
+
+dizerNome(nomeDoBancoDeDados);
+
+function soma(a, b) {
+    var soma = a + b;
+    return soma;
+}
+
+var somaUm = soma(2, 5);
+
+console.log(somaUm);
+
+var somaDois = soma(5, 300);
+
+console.log(somaDois);
+
+console.log(soma(4, 6));
