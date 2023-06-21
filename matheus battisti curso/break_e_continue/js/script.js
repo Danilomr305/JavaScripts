573ea833dbaa7ac70d04d321a8226bb27ad637c5
Danilomr305 <danilomr305@gmail.com>
2023-06-20 23:04:55 -0300

    .

--- matheus battisti curso/break_e_continue/js/script.js
+++ matheus battisti curso/break_e_continue/js/script.js
@@ -7,5 +7,21 @@ for(var i = 10; i > 0; i--) {
   }
 
 }
+// Break = parar 
 
 console.log("Deu o break");
+
+var x = 10;
+
+while(x < 100) {
+
+  x += 10;
+
+  if(x === 60) {
+    console.log("CONTINUE");
+    continue;
+  }
+
+  console.log("Testando continue " + x);
+
+}
