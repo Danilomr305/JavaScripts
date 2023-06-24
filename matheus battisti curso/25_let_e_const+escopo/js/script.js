f7f5a167c31af5cc07813814335694663fb5f4a7
Danilomr305 <danilomr305@gmail.com>
2023-06-24 19:24:42 -0300

    .

---
+++ matheus battisti curso/25_let_e_const+escopo/js/script.js
@@ -0,0 +1,22 @@
+//É só possivel um valor/ mais seguro para seu codigo
+const y = 89;
+
+console.log(y);
+
+
+//É possivel varios valores
+let x = 5;
+
+x = 12;
+
+console.log(x);
+
+if(true) {
+    
+    let x = 20;
+
+    console.log(x);
+
+}
+
+console.log(x);
